95e34e027bcaf02ccc5c3d56221a6194040a63a0
Steve Penrod <steve.penrod@mycroft.ai>
2019-01-06 02:54:02 -0600

    Initial status for CES 2019
    Final changes before burning images for CES.

--- home/aawadm/.config/autostart-scripts/StartupMycroft.sh
+++ home/aawadm/.config/autostart-scripts/StartupMycroft.sh
@@ -1,27 +1,31 @@
 #!/usr/bin/env bash
 
+# Move mouse from the center of the screen to the lower-right edge
+/usr/bin/xdotool mousemove 479 799
+
 # Unmute speakers (they boot muted to prevent crackling)
 /usr/local/bin/setgpio_dacamp_mute_off.sh
 /usr/local/bin/setgpio_line_mute_off.sh
 sleep 1
 
-# Get the IP address and conver to "10 dot 10 dot 14 dot 77" or whatever
-IP=$( ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*
-\.){3}[0-9]*' | grep -v '127.0.0.1' )
+# Get the IP address and convert to "10 dot 10 dot 14 dot 77" or whatever
+IP=$( ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1' )
 IPdotted=${IP//\./ dot }
-/home/aawadm/mycroft-core/mimic/bin/mimic -t "Hello.  I'm at I P address: $IPdotted"
+/home/aawadm/mycroft-core/mimic/bin/mimic -t "Hello. I'm Mycroft"
+sleep 1
+/home/aawadm/mycroft-core/mimic/bin/mimic -t "I'm at I P address: $IPdotted"
 
-# Check for updates
+# Check for remote updates
 bash /home/aawadm/CheckForUpdate.sh
 
-# Initializing power settings...
+# Initializing screen power settings...
 export DISPLAY=:0
 echo "on" | tee /sys/class/drm/card0/power/control
 echo "on" | tee /sys/class/drm/card0-DSI-1/power/control
 xset -dpms
 xset s off
 
-# Move mouse from the center of the screen to the lower-right edge
-# xdotool mousemove 479 799
+
+# Launch Mycroft Core and the Mycroft GUI
 sleep 5
 . /home/aawadm/start_2_mycroft.sh

b817edc2bec54d8b70c243e291ca03fd6d70a556
Steve Penrod <steve.penrod@mycroft.ai>
2019-01-06 02:28:38 -0600

    Test update and fix URL

--- home/aawadm/CheckForUpdate.sh
+++ home/aawadm/CheckForUpdate.sh
@@ -5,19 +5,20 @@ cd -P "$( dirname "$SOURCE" )"
 DIR="$( pwd )"
 
 # Download the latest update file from Github
-wget https://raw.githubusercontent.com/mycroft-dev/rootfs/home/aawadm/update.sh
+wget https://raw.githubusercontent.com/mycroft-dev/rootfs/master/home/aawadm/update.sh
+if [ -f update.sh ] ; then
+    if [ ! -f .last_update_md5 ] || ! md5sum -c &> /dev/null < .last_update_md5 ; then
+        #Store a fingerprint of update.sh
+        md5sum CheckForUpdate.sh update.sh > .last_update_md5
 
-if [ ! -f .last_update_md5 ] || ! md5sum -c &> /dev/null < .last_update_md5 ; then
-    #Store a fingerprint of update.sh
-    md5sum CheckForUpdate.sh update.sh > .last_update_md5
+        # Run the update
+        bash update.sh
 
-    # Run the update
-    bash update.sh
-
-    # Rename but keep track of it and when it was applied
-    cd $DIR
-    mv update.sh update.applied.$( date -Is )
-else
-    # Already applied, discard -- it will be downloaded again next boot
-    rm update.sh
+        # Rename but keep track of it and when it was applied
+        cd $DIR
+        mv update.sh update.applied.$( date -Is )
+    else
+        # Already applied, discard -- it will be downloaded again next boot
+        rm update.sh
+    fi
 fi
--- home/aawadm/update.sh
+++ home/aawadm/update.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # Just a test
-/home/aawadm/mycroft-core/mimic/bin/mimic -t "Running an update test"
+/home/aawadm/mycroft-core/mimic/bin/mimic -t "Running an update test 2"
